8ec38fb3c0b526475c208f6a29ca5f5cb099a686
Roman <romkatlt@list.ru>
2018-01-27 14:02:03 +0400

    Exchange between team images

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
     /**
      * Increase the score for Team A by 1 point.
      */
@@ -41,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
     public void addThreeForTeamA(View v) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
-
     }
 
     /**
@@ -50,31 +50,37 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
+        ImageView img = (ImageView) findViewById(R.id.ChicagoBulls);
+        img.setVisibility(View.VISIBLE);
+        img = (ImageView) findViewById(R.id.MiamiHeat);
+        img.setVisibility(View.GONE);
     }
 
+
     /**
-     * Increase the score for Team A by 1 point.
+     * Increase the score for Team B by 1 point.
      */
     public void addOneForTeamB(View v) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
+
+
     }
 
     /**
-     * Increase the score for Team A by 2 points.
+     * Increase the score for Team B by 2 points.
      */
     public void addTwoForTeamB(View v) {
         scoreTeamB = scoreTeamB + 2;
-     displayForTeamB(scoreTeamB);
+       displayForTeamB(scoreTeamB);
     }
 
     /**
-     * Increase the score for Team A by 3 points.
+     * Increase the score for Team B by 3 points.
      */
     public void addThreeForTeamB(View v) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
-
     }
 
     /**
@@ -85,15 +91,24 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamB = 0;
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
-
+        ImageView img = (ImageView) findViewById(R.id.ChicagoBulls);
+        img.setVisibility(View.GONE);
+        img = (ImageView) findViewById(R.id.MiamiHeat);
+        img.setVisibility(View.GONE);
     }
 
     /**
-     * Displays the given score for Team A.
+     * Displays the given score for Team B.
      */
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
+        ImageView img = (ImageView) findViewById(R.id.MiamiHeat);
+        img.setVisibility(View.VISIBLE);
+        img = (ImageView) findViewById(R.id.ChicagoBulls);
+        img.setVisibility(View.GONE);
+
+
     }
 
     public void ChicagoBulls(View view) {
